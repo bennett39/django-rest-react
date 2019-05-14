a95d8fd51d0593da4916672fdd9ddb649db36d06
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-05-14 17:51:52 -0400

    Implement REST API

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
 from django.http import HttpResponse
+
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
+
 
 def index(request):
     return HttpResponse("Hello World!")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ View & edit users via API endpoint """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """ View & edit groups via API endpoint """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
