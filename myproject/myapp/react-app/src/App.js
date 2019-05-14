1b08e4417cae6384228387d7a70d2eff15a10d59
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-05-14 18:24:36 -0400

    Create form in React

--- myproject/myapp/react-app/src/App.js
+++ myproject/myapp/react-app/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form action="http://127.0.0.1:8000/users/" method="POST" enctype="multipart/form-data" className="form-horizontal" novalidate>
+        <input type="hidden" name="csrfmiddlewaretoken" value="crIlEX8FyZlkOQEst6pdbZFCJ7NCQWlY29jxRS71n7dVhAtF590jb91PocyCGr7u" />
+    <br />
+        <label>Username
+          <input name="username" type="text" />
+        </label>
+    <br />
+        <label>Email address
+          <input name="email" className="form-control" type="email" />
+        </label>
+    <br />
+        <label>Groups
+          <select multiple  className="form-control" >
+            <option value="http://127.0.0.1:8000/groups/1/">test_group</option>
+          </select>
+        </label>
+    <br />
+        <button className="btn btn-primary js-tooltip" title="Make a POST request on the User List resource">POST</button>
+      </form>
+
     </div>
   );
 }
